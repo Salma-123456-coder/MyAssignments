7d36e99a611f28d49c0bdf2544d50499a90ea95c
Salma-123456-coder <ssultana1289@gmail.com>
2026-02-05 19:27:50 +0530

    Add files via upload
    Week1-day1 assignments

---
+++ SalmaWeek1/day1/Classwithmain.java
@@ -0,0 +1,12 @@
+package SalmaWeek1.day1;
+
+public class Classwithmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Here is my first code");
+
+	}
+
+}
---
+++ SalmaWeek1/day1/Classwithoutmain.java
@@ -0,0 +1,5 @@
+package SalmaWeek1.day1;
+
+public class Classwithoutmain {
+
+}
